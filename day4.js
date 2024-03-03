7f1d53bb44043dd369213b1e49aa29b21f37c60c
Leticia Matie <matieleticia@gmail.com>
2024-03-03 11:01:38 -0300

    day 4

--- day4.js
+++ day4.js
@@ -41,3 +41,25 @@ function numeroAleatorio() {
   if (tentativa > numTentativas) {
     alert(`Você perdeu. O número era ${adivinharNumero}.`);
   }
+
+/*
+RESPOSTA:
+
+const numeroAdivinhacao = Math.floor(Math.random() * (10 - 0 + 1) + 0);
+let chute = "";
+let acertou = false;
+
+for(let contador = 0; contador < 3; contador++){    
+    chute = prompt("Tente adivinhar o número de 0 a 10:");
+    if(chute == numeroAdivinhacao){
+        alert(`Parabéns, você acertou! O número era ${numeroAdivinhacao}.`);
+        acertou = true;
+        break;
+    }
+    alert("Errado!");
+}
+if(!acertou){
+    alert(`Infelizmente, você não acertou. O número era ${numeroAdivinhacao}!`);
+}
+
+*/
